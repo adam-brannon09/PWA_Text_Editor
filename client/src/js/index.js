0b3de3eac5672248ca2613d10932d3aaf1fe994d
adambrannon09-jhKgEp <66850339+adam-brannon09@users.noreply.github.com>
2023-07-25 22:31:42 -0500

    havent pushed in a few days. cant remember where i left off. index, htmlRoutes, and server all have changes, probably comments, some functionality.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,11 +1,22 @@
+
+// Import the Workbox object from the 'workbox-window' module for service worker registration
 import { Workbox } from 'workbox-window';
+
+// Import the Editor class from the 'editor' module
 import Editor from './editor';
+
+// Import the 'database' module, which is assumed to be a script that sets up a database (no objects or functions are imported from it)
 import './database';
+
+// Import the 'style.css' stylesheet
 import '../css/style.css';
 
+// Select the 'main' element from the DOM
 const main = document.querySelector('#main');
+// Clear any existing HTML inside the 'main' element
 main.innerHTML = '';
 
+// Define a function to create and append a loading spinner to the 'main' element
 const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
@@ -14,20 +25,25 @@ const loadSpinner = () => {
   <div class="loading-spinner" />
   </div>
   `;
+  // Append the spinner to the 'main' element
   main.appendChild(spinner);
 };
 
+// Create a new instance of the Editor class
 const editor = new Editor();
 
+// Check if the editor instance is 'undefined' and if it is then load the spinner
 if (typeof editor === 'undefined') {
   loadSpinner();
 }
 
-// Check if service workers are supported
+// Check if service workers are supported in the current browser
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
+  // Create a new Workbox instance with the '/src-sw.js' service worker file
   const workboxSW = new Workbox('/src-sw.js');
+  // Register the service worker
   workboxSW.register();
 } else {
+  // Log an error message to the console if service workers are not supported
   console.error('Service workers are not supported in this browser.');
 }
--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,12 @@
+  // Import the 'path' module, which provides utilities for working with file and directory paths
 const path = require('path');
 
+// Export a function that takes an Express application as an argument
 module.exports = (app) =>
+  // Register a GET handler for the root URL ("/")
   app.get('/', (req, res) =>
+    // Respond to the request by sending the 'index.html' file
+    // Use the 'path.join' function to create a path to the 'index.html' file
+    // '__dirname' is the directory where the current script resides, so we go up one level ('../') to get to its parent directory, then down into the 'client/dist' directory
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );
